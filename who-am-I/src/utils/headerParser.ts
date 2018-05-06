f784e075428ce2d5633d7314eae2b4d248dde117
nosferatoy <giorgospapa86@gmail.com>
2018-05-06 18:19:47 +0200

    add ip parser and rename folder

--- who-am-I/src/utils/headerParser.ts
+++ who-am-I/src/utils/headerParser.ts
@@ -6,13 +6,15 @@ export function getWhoAmI (req: Request): WhoAmI {
 
     let userAgent = req.headers["user-agent"] || "";
     let languageHeader = req.headers["accept-language"] || "";
-
-    let ipaddress = req.headers['x-forwarded-for'] || 
+    let ipHeader = req.headers['x-forwarded-for'] || 
     req.connection.remoteAddress || 
     req.socket.remoteAddress || "" ;
+
+    console.log(req.connection.remoteAddress);
     let language = languageHeader.slice(0, languageHeader.indexOf(","));
     let software = userAgent.slice(userAgent.indexOf("(") + 1, userAgent.indexOf(")"));
-    
+    let ipaddress = ipHeader.slice(ipHeader.lastIndexOf(":") + 1, ipHeader.length);
+
     let result = {
         ipaddress: ipaddress,
         language: language,
